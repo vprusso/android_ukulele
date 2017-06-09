0b7fbb0409e180655dd4d11d188b721c0b39e041
Vincent Russo <vincentrusso1@gmail.com>
2017-06-08 23:20:48 -0400

    Starting to add chord sequencer functionaltiy

--- UkuleleChords/app/src/main/java/com/example/captainhampton/ukulelechords/SequencerActivity.java
+++ UkuleleChords/app/src/main/java/com/example/captainhampton/ukulelechords/SequencerActivity.java
@@ -12,21 +12,26 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SequencerActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     private UkuleleChordUtil ukuleleChordUtil = new UkuleleChordUtil();
-
-    // Spinner Note elements
-    private Spinner spinnerNote1 = (Spinner)findViewById(R.id.spinnerNote1);
-
-    private ImageView imageViewChord1 = (ImageView)findViewById(R.id.imageViewChord1);
+    private HashMap<String, Integer> chordDrawableHashMap = ukuleleChordUtil.getSelectedChordDrawableHashMap();
+    private HashMap<String, Integer> chordRawHashMap = ukuleleChordUtil.getSelectedChordRawHashMap();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sequencer);
+
+        // Spinner Note elements
+        Spinner spinnerNote1 = (Spinner)findViewById(R.id.spinnerNote1);
+        Spinner spinnerNote2 = (Spinner)findViewById(R.id.spinnerNote2);
+
+        //final ImageView imageViewChord1 = (ImageView)findViewById(R.id.imageViewChord1);
+        //final ImageView imageViewChord2 = (ImageView)findViewById(R.id.imageViewChord2);
 
         // Spinner Drop down elements
         String[] notes = {"A", "A#", "B", "C", "C#", "D", "D#", "E",
@@ -34,11 +39,14 @@ public class SequencerActivity extends Activity implements AdapterView.OnItemSel
 
         // Creating adapter for spinner
         ArrayAdapter<String> noteAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, notes);
+        ArrayAdapter<String> noteAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, notes);
 
         // Drop down layout style - list view with radio button
         noteAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        noteAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinnerNote1.setAdapter(noteAdapter1);
+        spinnerNote2.setAdapter(noteAdapter2);
 
         spinnerNote1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -49,14 +57,54 @@ public class SequencerActivity extends Activity implements AdapterView.OnItemSel
                 Spinner spinnerChord1 = (Spinner)findViewById(R.id.spinnerChord1);
                 String [] chords = ukuleleChordUtil.createUkuleleChords(selected_tuning);
 
-                ArrayAdapter<String> chordAdapter = new ArrayAdapter<String>(SequencerActivity.this, android.R.layout.simple_spinner_item, chords);
+                ArrayAdapter<String> chordAdapter1 = new ArrayAdapter<String>(SequencerActivity.this, android.R.layout.simple_spinner_item, chords);
 
-                chordAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinnerChord1.setAdapter(chordAdapter);
+                chordAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerChord1.setAdapter(chordAdapter1);
                 spinnerChord1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                         String selected_tuning = parent.getItemAtPosition(position).toString();
+                        ImageView imageViewChord1 = (ImageView)findViewById(R.id.imageViewChord1);
+                        imageViewChord1.setImageResource(chordDrawableHashMap.get(selected_tuning));
+
+                        Toast.makeText(parent.getContext(), "Selected: " + selected_tuning, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerNote2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected_tuning = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + selected_tuning, Toast.LENGTH_LONG).show();
+
+                Spinner spinnerChord2 = (Spinner)findViewById(R.id.spinnerChord2);
+                String [] chords = ukuleleChordUtil.createUkuleleChords(selected_tuning);
+
+                ArrayAdapter<String> chordAdapter2 = new ArrayAdapter<String>(SequencerActivity.this, android.R.layout.simple_spinner_item, chords);
+
+                chordAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerChord2.setAdapter(chordAdapter2);
+                spinnerChord2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        String selected_tuning = parent.getItemAtPosition(position).toString();
+                        ImageView imageViewChord2 = (ImageView)findViewById(R.id.imageViewChord2);
+                        imageViewChord2.setImageResource(chordDrawableHashMap.get(selected_tuning));
+
                         Toast.makeText(parent.getContext(), "Selected: " + selected_tuning, Toast.LENGTH_LONG).show();
                     }
 
@@ -78,17 +126,7 @@ public class SequencerActivity extends Activity implements AdapterView.OnItemSel
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Spinner spinner = (Spinner) parent;
 
-
-        if (spinner.getId() == R.id.spinnerChord1) {
-        }
-
-        //String selected_tuning = parent.getItemAtPosition(position).toString();
-        //setTuning(selected_tuning);
-
-        // Showing selected spinner item
-        //Toast.makeText(parent.getContext(), "Selected: " + selected_tuning, Toast.LENGTH_LONG).show();
     }
 
     @Override
